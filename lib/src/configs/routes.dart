8608271548a97052548dded2e1ec5919c06eb9bd
Pedro Belfort <belfortdev@gmail.com>
2020-07-15 20:06:27 -0300

    Update routes

--- lib/src/configs/routes.dart
+++ lib/src/configs/routes.dart
@@ -1,6 +1,12 @@
+import 'package:dr_app/src/screens/category_detail_screen.dart';
 import 'package:dr_app/src/screens/home_screen.dart';
+import 'package:dr_app/src/screens/outlet_detail_screen.dart';
+import 'package:dr_app/src/screens/scanner_screen.dart';
 import 'package:flutter/widgets.dart';
 
 final Map<String, WidgetBuilder> routes = {
-  HomeScreen.id: (context) => HomeScreen()
+  HomeScreen.id: (context) => HomeScreen(),
+  ScannerScreen.id: (context) => ScannerScreen(),
+  OutletDetailScreen.id: (context) => OutletDetailScreen(),
+  CategoryDetailScreen.id: (context) => CategoryDetailScreen(),
 };
