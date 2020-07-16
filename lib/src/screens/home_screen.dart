2f18b1242a07a555d26d25d6c8e18be689036c51
Pedro Belfort <belfortdev@gmail.com>
2020-07-16 18:47:44 -0300

    Create SliderButton component

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,24 +1,6 @@
 //import 'package:dr_app/src/components/buttons/solid_button.dart';
-import 'package:dr_app/src/components/buttons/outline_button.dart';
+import 'package:dr_app/src/components/buttons/slide_button.dart';
 import 'package:flutter/material.dart';
-
-//class HomeScreen extends StatelessWidget {
-//  static const id = 'home_screen';
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//      body: Container(
-//        child: Center(
-//            child: SolidButton(
-//          title: 'Hello World',
-//          color: Color(0xFF4F5D75),
-//          onPressed: () {},
-//        )),
-//      ),
-//    );
-//  }
-//}
 
 class HomeScreen extends StatelessWidget {
   static const id = 'home_screen';
@@ -28,9 +10,12 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: Container(
         child: Center(
-            child: LUOutlineButton(
-          onPressed: () {},
-          title: 'Hello World',
+            child: LUSliderButton(
+          title: "check-in",
+          semanticTitle: 'check-in',
+          onSlided: () {
+            print('Slided!');
+          },
         )),
       ),
     );
