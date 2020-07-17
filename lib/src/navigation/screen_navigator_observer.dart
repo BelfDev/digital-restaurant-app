ca57b120d89c43d1b4c108bf0d397fd92709a356
Pedro Belfort <belfortdev@gmail.com>
2020-07-17 18:51:32 -0300

    Create NavigatorObserver

---
+++ lib/src/navigation/screen_navigator_observer.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ViewNavigatorObserver extends NavigatorObserver {
+  ViewNavigatorObserver(this.onNavigation);
+
+  final VoidCallback onNavigation;
+
+  void didPop(Route<dynamic> route, Route<dynamic> previousRoute) {
+    onNavigation();
+  }
+
+  void didPush(Route<dynamic> route, Route<dynamic> previousRoute) {
+    onNavigation();
+  }
+}
