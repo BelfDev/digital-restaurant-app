69a639f76ce8b2814cde7f24528006e1d2792e39
Pedro Belfort <belfortdev@gmail.com>
2020-07-17 18:50:46 -0300

    Configure app tabs

---
+++ lib/src/navigation/tabs.dart
@@ -0,0 +1,13 @@
+import 'package:dr_app/src/navigation/tab_data.dart';
+import 'package:dr_app/src/screens/explore_screen.dart';
+import 'package:dr_app/src/screens/home_screen.dart';
+import 'package:dr_app/src/screens/more_screen.dart';
+import 'package:dr_app/src/screens/profile_screen.dart';
+import 'package:flutter/material.dart';
+
+const List<TabData> tabs = <TabData>[
+  TabData(0, HomeScreen.id, 'Home', Icons.restaurant, Colors.teal),
+  TabData(1, ExploreScreen.id, 'Explore', Icons.explore, Colors.cyan),
+  TabData(2, ProfileScreen.id, 'Profile', Icons.person_pin, Colors.orange),
+  TabData(3, MoreScreen.id, 'More', Icons.more_horiz, Colors.blue)
+];
