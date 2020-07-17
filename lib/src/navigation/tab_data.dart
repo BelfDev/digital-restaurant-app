ee219c4a539443a82cb357c573868617ee9dfb53
Pedro Belfort <belfortdev@gmail.com>
2020-07-17 18:50:25 -0300

    Create TabData navigation model

---
+++ lib/src/navigation/tab_data.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class TabData {
+  final int index;
+  final String rootId;
+  final String title;
+  final IconData icon;
+  final MaterialColor color;
+
+  const TabData(this.index, this.rootId, this.title, this.icon, this.color);
+}
