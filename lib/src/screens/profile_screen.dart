eb3deec1934da131e12031433fd4a0a20096646c
Pedro Belfort <belfortdev@gmail.com>
2020-07-17 15:04:29 -0300

    Create profile_screen scaffolding

---
+++ lib/src/screens/profile_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  static const id = 'profile_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Center(child: Text('Profile Screen')));
+  }
+}
