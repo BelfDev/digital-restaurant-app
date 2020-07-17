d03e94a5d3b6881260cb73a27a01d27215faaf5a
Pedro Belfort <belfortdev@gmail.com>
2020-07-17 18:52:44 -0300

    Set the application scaffolding background to white

--- lib/src/configs/theme.dart
+++ lib/src/configs/theme.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 ThemeData getAppTheme() => ThemeData(
       primarySwatch: Colors.blue,
       visualDensity: VisualDensity.adaptivePlatformDensity,
-      scaffoldBackgroundColor: Colors.green,
+      scaffoldBackgroundColor: Colors.white,
     );
