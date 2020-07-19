f7936a2fdb8cfe5bbddf3bba92599af847c46f0d
Pedro Belfort <belfortdev@gmail.com>
2020-07-19 19:06:20 -0300

    Create HomeScreen background shapes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,3 @@
-import 'package:dr_app/components/cards/category_card.dart';
-import 'package:dr_app/components/cards/featured_card.dart';
 import 'package:dr_app/components/cards/outlet_card.dart';
 import 'package:flutter/material.dart';
 
@@ -10,38 +8,79 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
+//class _HomeScreenState extends State<HomeScreen> {
+//  @override
+//  Widget build(BuildContext context) {
+//    return SafeArea(
+//      child: Scaffold(
+//        body: Container(
+//            child: Column(
+//          children: <Widget>[
+//            LUFeaturedCard(
+//              imageSrc: 'https://placeimg.com/640/480/any',
+//              onPressed: () {
+//                print('tapped');
+//              },
+//              title: 'Bar Soba',
+//              subtitle: 'Asian Food',
+//              priceRange: '\$\$',
+//              rating: 4,
+//            ),
+//            LUCategoryCard(
+//              title: 'Italian',
+//              onPressed: () {
+//                print('Pressed on Category Card');
+//              },
+//            ),
+//            LUOutletCard(
+//              title: 'La Vita Sputini',
+//              rating: 4.0,
+//              priceRange: '\$\$',
+//              onPressed: () {},
+//            ),
+//          ],
+//        )),
+//      ),
+//    );
+//  }
+//}
+
 class _HomeScreenState extends State<HomeScreen> {
+  final dummyCards = [1, 2, 3, 4, 5];
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        backgroundColor: Color(0xFFFAFAFA),
         body: Container(
-            child: Column(
-          children: <Widget>[
-            LUFeaturedCard(
-              imageSrc: 'https://placeimg.com/640/480/any',
-              onPressed: () {
-                print('tapped');
-              },
-              title: 'Bar Soba',
-              subtitle: 'Asian Food',
-              priceRange: '\$\$',
-              rating: 4,
+          margin: EdgeInsets.only(top: 90),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(40), topRight: Radius.circular(40)),
+            color: Colors.yellow,
+          ),
+          child: Container(
+            margin: EdgeInsets.only(top: 180),
+            decoration: BoxDecoration(
+              color: Color(0xFFFAFAFA),
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40), topRight: Radius.circular(40)),
             ),
-            LUCategoryCard(
-              title: 'Italian',
-              onPressed: () {
-                print('Pressed on Category Card');
-              },
+            child: ListView(
+              children: dummyCards
+                  .map(
+                    (e) => LUOutletCard(
+                      title: 'La Vita Sputini',
+                      rating: 4.0,
+                      priceRange: '\$\$',
+                      onPressed: () {},
+                    ),
+                  )
+                  .toList(),
             ),
-            LUOutletCard(
-              title: 'La Vita Sputini',
-              rating: 4.0,
-              priceRange: '\$\$',
-              onPressed: () {},
-            ),
-          ],
-        )),
+          ),
+        ),
       ),
     );
   }
