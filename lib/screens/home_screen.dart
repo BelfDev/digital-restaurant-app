926c480f20abaff16d50425cb0fd063e8066cfca
Pedro Belfort <belfortdev@gmail.com>
2020-07-19 19:30:48 -0300

    Add custom HomeScreen bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,4 @@
-import 'package:dr_app/components/cards/outlet_card.dart';
+import 'package:dr_app/components/buttons/icon_button.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -45,42 +45,127 @@ class HomeScreen extends StatefulWidget {
 //  }
 //}
 
+//class _HomeScreenState extends State<HomeScreen> {
+//  final dummyCards = [1, 2, 3, 4, 5];
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return SafeArea(
+//      child: Scaffold(
+//        backgroundColor: Color(0xFFFAFAFA),
+//        body: Container(
+//          margin: EdgeInsets.only(top: 90),
+//          decoration: BoxDecoration(
+//            borderRadius: BorderRadius.only(
+//                topLeft: Radius.circular(40), topRight: Radius.circular(40)),
+//            color: Colors.yellow,
+//          ),
+//          child: Container(
+//            margin: EdgeInsets.only(top: 180),
+//            decoration: BoxDecoration(
+//              color: Color(0xFFFAFAFA),
+//              borderRadius: BorderRadius.only(
+//                  topLeft: Radius.circular(40), topRight: Radius.circular(40)),
+//            ),
+//            child: ListView(
+//              children: dummyCards
+//                  .map(
+//                    (e) => LUOutletCard(
+//                      title: 'La Vita Sputini',
+//                      rating: 4.0,
+//                      priceRange: '\$\$',
+//                      onPressed: () {},
+//                    ),
+//                  )
+//                  .toList(),
+//            ),
+//          ),
+//        ),
+//      ),
+//    );
+//  }
+//}
+
+//class _HomeScreenState extends State<HomeScreen> {
+//  final dummyCards = [1, 2, 3, 4, 5];
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      backgroundColor: Color(0xFFFAFAFA),
+//      body: SingleChildScrollView(
+//        child: Container(
+//          height: 1000,
+//          margin: EdgeInsets.only(top: 90),
+//          decoration: BoxDecoration(
+//            borderRadius: BorderRadius.only(
+//                topLeft: Radius.circular(40), topRight: Radius.circular(40)),
+//            color: Colors.yellow,
+//          ),
+//          child: Container(
+//            margin: EdgeInsets.only(top: 180),
+//            decoration: BoxDecoration(
+////              color: Color(0xFFFAFAFA),
+//              color: Colors.blueGrey,
+//              borderRadius: BorderRadius.only(
+//                  topLeft: Radius.circular(40), topRight: Radius.circular(40)),
+//            ),
+//          ),
+//        ),
+//      ),
+//    );
+//  }
+//}
+
 class _HomeScreenState extends State<HomeScreen> {
   final dummyCards = [1, 2, 3, 4, 5];
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Color(0xFFFAFAFA),
-        body: Container(
-          margin: EdgeInsets.only(top: 90),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(40), topRight: Radius.circular(40)),
-            color: Colors.yellow,
+    return Scaffold(
+      backgroundColor: Color(0xFFFAFAFA),
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Le Serveur',
+                  style: TextStyle(color: Colors.black, fontSize: 40),
+                ),
+                LUIconButton(
+                  icon: Icons.center_focus_strong,
+                  iconSize: 32,
+                  onPressed: () {
+                    print('Pressed On Scanner');
+                  },
+                )
+              ],
+            ),
           ),
-          child: Container(
-            margin: EdgeInsets.only(top: 180),
+          Container(
+            height: 1000,
+            margin: EdgeInsets.only(top: 32),
             decoration: BoxDecoration(
-              color: Color(0xFFFAFAFA),
               borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(40), topRight: Radius.circular(40)),
+              color: Colors.yellow,
             ),
-            child: ListView(
-              children: dummyCards
-                  .map(
-                    (e) => LUOutletCard(
-                      title: 'La Vita Sputini',
-                      rating: 4.0,
-                      priceRange: '\$\$',
-                      onPressed: () {},
-                    ),
-                  )
-                  .toList(),
+            child: Container(
+              margin: EdgeInsets.only(top: 180),
+              decoration: BoxDecoration(
+//              color: Color(0xFFFAFAFA),
+                color: Colors.blueGrey,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40)),
+              ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
