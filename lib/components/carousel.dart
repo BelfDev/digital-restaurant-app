c5c90ad4c647dc6aa97b8b32dea0094d34efbda1
Pedro Belfort <belfortdev@gmail.com>
2020-07-22 13:20:56 +0100

    Adjust Carousel horizontal scroll physics

--- lib/components/carousel.dart
+++ lib/components/carousel.dart
@@ -23,7 +23,7 @@ class LUCarousel extends StatelessWidget {
       height: height,
       child: ListView.separated(
         padding: padding,
-        shrinkWrap: true, //just set this property
+        physics: BouncingScrollPhysics(),
         scrollDirection: Axis.horizontal,
         itemCount: children.length,
         separatorBuilder: (BuildContext context, int index) => VerticalDivider(
