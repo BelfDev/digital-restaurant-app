0ea390c29bd57c9f49bf6021a73ef341022156f7
Pedro Belfort <belfortdev@gmail.com>
2020-07-26 14:05:57 +0100

    Customize swiper pagination

--- lib/components/swiper.dart
+++ lib/components/swiper.dart
@@ -1,3 +1,5 @@
+import 'package:dr_app/configs/theme.dart';
+import 'package:dr_app/utils/images.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_page_indicator/flutter_page_indicator.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
@@ -13,15 +15,22 @@ class LUSwiper extends StatelessWidget {
   Widget build(BuildContext context) {
     return Swiper(
       itemBuilder: (BuildContext context, int index) {
-        return Image.network(
-          images[index],
-          fit: BoxFit.fill,
+        return FadeInImage.assetNetwork(
+          placeholder: Images.horizontalPlaceholder,
+          image: images[index],
+          fit: BoxFit.cover,
         );
       },
       indicatorLayout: PageIndicatorLayout.SCALE,
       autoplay: false,
       itemCount: images.length,
-      pagination: SwiperPagination(),
+      outer: true,
+      pagination: SwiperPagination(
+          margin: EdgeInsets.only(top: 16),
+          builder: DotSwiperPaginationBuilder(
+              space: 8.0,
+              color: LUTheme.of(context).unselectedWidgetColor,
+              activeColor: LUTheme.of(context).primaryColor)),
     );
   }
 }
