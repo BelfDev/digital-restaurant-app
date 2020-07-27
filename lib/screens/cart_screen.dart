5dd37b596b2283a22b3772827fc2fceefe38c5ed
Pedro Belfort <belfortdev@gmail.com>
2020-07-27 18:02:42 +0100

    Create Cart screen scaffolding and register its route

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
-class CartScreen extends StatelessWidget {
+class CartScreen extends StatefulWidget {
   static const id = 'cart_screen';
+  @override
+  _CartScreenState createState() => _CartScreenState();
+}
 
+class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(child: Center(child: Text('Cart Screen')));
+    return Container(
+      color: Colors.blue,
+    );
   }
 }
