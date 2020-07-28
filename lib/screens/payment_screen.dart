c2c4191800f184ba0ac4dee809797f4866e33a09
Pedro Belfort <belfortdev@gmail.com>
2020-07-28 17:55:10 +0100

    Add bottom sliver

--- lib/screens/payment_screen.dart
+++ lib/screens/payment_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dr_app/components/bottom_sliver.dart';
 import 'package:dr_app/components/top_bar.dart';
 import 'package:dr_app/configs/theme.dart';
 import 'package:flutter/material.dart';
@@ -13,18 +14,29 @@ class _PaymentScreenState extends State<PaymentScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: Stack(
         children: <Widget>[
-          LUTopBar(
-            title: 'Payment',
-            buttonBackgroundColor: LUTheme.of(context).primaryColor,
-            tint: LUTheme.of(context).backgroundColor,
-            onNavigationButtonPressed: () => Navigator.of(context).pop(),
+          Column(
+            children: <Widget>[
+              LUTopBar(
+                title: 'Payment',
+                buttonBackgroundColor: LUTheme.of(context).primaryColor,
+                tint: LUTheme.of(context).backgroundColor,
+                onNavigationButtonPressed: () => Navigator.of(context).pop(),
+              ),
+              Container(
+                color: Colors.red,
+                height: 400,
+              ),
+            ],
           ),
-          Container(
-            color: Colors.red,
-            height: 400,
-          )
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: LUBottomSliver(
+              buttonTitle: 'pay',
+              onButtonPressed: () => Navigator.of(context).pop(),
+            ),
+          ),
         ],
       ),
     );
