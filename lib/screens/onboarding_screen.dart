2927176d8b904a667843876ed6f2f5f31f393ce7
Pedro Belfort <belfortdev@gmail.com>
2020-08-08 15:39:43 +0100

    Add slides

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -19,108 +19,82 @@ class OnboardingScreen extends StatefulWidget {
   _OnboardingScreenState createState() => _OnboardingScreenState();
 }
 
-//class _OnboardingScreenState extends State<OnboardingScreen> {
-//  @override
-//  Widget build(BuildContext context) {
-//    return Container(
-//      color: LUTheme.of(context).backgroundColor,
-//      child: SafeArea(
-//          child: Column(
-//        children: <Widget>[
-//          Text('NICE'),
-//          LUSolidButton(
-//            title: 'Go to app',
-//            onPressed: () => Navigator.pushAndRemoveUntil(
-//                context,
-//                MaterialPageRoute(builder: (context) => RootContainer()),
-//                (_) => false),
-//          )
-//        ],
-//      )),
-//    );
-//  }
-//}
-
 class _OnboardingScreenState extends State<OnboardingScreen> {
+  final List<Slide> slides = <Slide>[
+    Slide(
+      text: 'Find the perfect restaurant for your taste',
+      imgSrc: Images.introSlide1,
+      borderRadius: BorderRadius.only(bottomLeft: Radius.circular(40.0)),
+    ),
+    Slide(
+      text: 'Place your orders from within the app',
+      imgSrc: Images.introSlide2,
+    ),
+    Slide(
+      text: 'Pay and leave whenever you please',
+      imgSrc: Images.introSlide3,
+      borderRadius: BorderRadius.only(bottomRight: Radius.circular(40.0)),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: LUTheme.of(context).backgroundColor,
         body: Column(
           children: <Widget>[
-//            Expanded(flex: 7, child: LUSwiper(imgSrcList: dummySwiperImages)),
-            Expanded(flex: 7, child: IntroSlider()),
-            Expanded(flex: 3, child: buildButtons())
+            Expanded(
+                flex: 3,
+                child: IntroSlider(
+                  slides: slides,
+                )),
+            Expanded(flex: 1, child: buildButtons())
           ],
         ));
   }
 
-  Widget buildButtons() => Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            LUSolidButton(
-              title: "Login",
-              onPressed: () => Navigator.of(context).pushNamed(LoginScreen.id),
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            LUOutlineButton(
-              title: "Skip",
-              onPressed: () => Navigator.pushAndRemoveUntil(
-                  context,
-                  MaterialPageRoute(builder: (context) => RootContainer()),
-                  (_) => false),
-            )
-          ],
+  Widget buildButtons() => SafeArea(
+        top: false,
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              LUSolidButton(
+                title: "Login",
+                onPressed: () =>
+                    Navigator.of(context).pushNamed(LoginScreen.id),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              LUOutlineButton(
+                title: "Skip",
+                onPressed: () => Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => RootContainer()),
+                    (_) => false),
+              )
+            ],
+          ),
         ),
       );
 }
 
 class IntroSlider extends StatelessWidget {
+  final List<Slide> slides;
+
+  const IntroSlider({Key key, @required this.slides}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Swiper(
       itemBuilder: (BuildContext context, int index) {
-        return AnimatedContainer(
-          decoration: BoxDecoration(
-              color: LUColors.yellow,
-              borderRadius: BorderRadius.circular(32.0)),
-          duration: Duration(milliseconds: 240),
-          child: SafeArea(
-            child: Column(
-              children: <Widget>[
-                Container(
-                  padding:
-                      EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
-                  decoration: BoxDecoration(
-                      color: LUColors.darkYellow.withOpacity(0.15),
-                      borderRadius: BorderRadius.circular(16.0)),
-                  child: Image.asset(
-                    Images.appLogo,
-                    width: 227,
-                    height: 46,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                Container(
-                  color: Colors.blue,
-                  height: 200,
-                ),
-                Text(
-                  "Find the perfect restaurant for your taste",
-                  style: Styles.introText,
-                ),
-              ],
-            ),
-          ),
-        );
+        return slides[index];
       },
       indicatorLayout: PageIndicatorLayout.SCALE,
       loop: false,
       autoplay: false,
-      itemCount: 1,
+      itemCount: slides.length,
       outer: true,
       index: 0,
       pagination: SwiperPagination(
@@ -129,6 +103,56 @@ class IntroSlider extends StatelessWidget {
               space: 8.0,
               color: LUTheme.of(context).unselectedWidgetColor,
               activeColor: LUTheme.of(context).primaryColor)),
+    );
+  }
+}
+
+class Slide extends StatelessWidget {
+  final String text;
+  final String imgSrc;
+  final BorderRadiusGeometry borderRadius;
+
+  const Slide({
+    Key key,
+    @required this.text,
+    @required this.imgSrc,
+    this.borderRadius,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration:
+          BoxDecoration(color: LUColors.yellow, borderRadius: borderRadius),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 48, vertical: 40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
+              decoration: BoxDecoration(
+                  color: LUColors.darkYellow.withOpacity(0.15),
+                  borderRadius: BorderRadius.circular(16.0)),
+              child: Image.asset(
+                Images.appLogo,
+                width: 227,
+                height: 46,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Image.asset(
+              imgSrc,
+              fit: BoxFit.cover,
+            ),
+            Text(
+              text,
+              textAlign: TextAlign.center,
+              style: Styles.introText,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/utils/images.dart
+++ lib/utils/images.dart
@@ -5,4 +5,7 @@ abstract class Images {
   static const homeChef = 'res/images/home-chef.png';
   static const homeHeaderBackground = 'res/images/header-bg.png';
   static const appLogo = 'res/images/logo.png';
+  static const introSlide1 = 'res/images/intro-1.png';
+  static const introSlide2 = 'res/images/intro-2.png';
+  static const introSlide3 = 'res/images/intro-3.png';
 }
