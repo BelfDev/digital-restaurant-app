e81ecb3e6d3cd39e16f6e8a498f6d1eb7aca1723
Pedro Belfort <belfortdev@gmail.com>
2020-08-13 22:16:05 +0100

    Inject the cuisine repository

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,38 @@
+import 'package:bloc/bloc.dart';
+import 'package:dr_app/blocs/app_bloc_observer.dart';
+import 'package:dr_app/blocs/bloc.dart';
 import 'package:dr_app/configs/routes.dart';
+import 'package:dr_app/data/repositories/cuisine_repo.dart';
 import 'package:dr_app/screens/onboarding_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'configs/theme.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  Bloc.observer = AppBlocObserver();
+
+  final CuisineRepo cuisineRepo = CuisineRepo();
+
+  // Injects all available repos
+  runApp(MyApp(
+    cuisineRepo: cuisineRepo,
+  ));
+}
 
 class MyApp extends StatelessWidget {
+  final CuisineRepo cuisineRepo;
+
+  const MyApp({Key key, @required this.cuisineRepo}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: LUTheme.of(context),
-      home: OnboardingScreen(),
+      home: BlocProvider(
+        create: (context) => HomeBloc(cuisineRepo: cuisineRepo),
+        child: OnboardingScreen(),
+      ),
       routes: routes,
     );
   }
