779ff1061a601c4909d1407d06b4b3edcbd73dfd
Pedro Belfort <belfortdev@gmail.com>
2020-08-17 21:14:53 +0100

    Create outlet repository

---
+++ lib/data/repositories/outlet_respository.dart
@@ -0,0 +1,27 @@
+import 'package:dr_app/data/models/remote/outlet.dart';
+import 'package:dr_app/services/api/outlet_api_client.dart';
+
+/// This class is the only data source for the Outlet entity.
+/// It should be seen as the single source of truth for fetching
+/// or storing data.
+class OutletRepository {
+  final _outletApiService = OutletApiClient();
+
+  /// Retrieves all stored Outlet entities
+  Future<List<Outlet>> fetchAllOutlets([String city]) async {
+    final body = await _outletApiService.getOutlets(city);
+    return body.results;
+  }
+
+  /// Retrieves all stored Outlet entities
+  Future<List<Outlet>> fetchAllFeaturedOutlets([String city]) async {
+    final body = await _outletApiService.getFeaturedOutlets(city);
+    return body.results;
+  }
+
+  /// Retrieves a single Outlet entity by the given identifier
+  Future<Outlet> fetchOutlet(int id) async {
+    final body = await _outletApiService.getOutletById(id);
+    return body.results.length > 0 ? body.results[0] : null;
+  }
+}
