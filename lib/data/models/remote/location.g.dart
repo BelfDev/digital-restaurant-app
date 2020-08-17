3dde9cbcb0a865ea4abe3fa6dd1ea887a5d61e86
Pedro Belfort <belfortdev@gmail.com>
2020-08-17 21:13:37 +0100

    Create location remote model

---
+++ lib/data/models/remote/location.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'location.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Location _$LocationFromJson(Map<String, dynamic> json) {
+  return Location(
+    json['city'] as String,
+  );
+}
+
+Map<String, dynamic> _$LocationToJson(Location instance) => <String, dynamic>{
+      'city': instance.city,
+    };
