4624ba293ef9ff222da5ee1f347b2f933a4c0b53
Pedro Belfort <belfortdev@gmail.com>
2020-09-01 17:23:06 -0300

    Create models barrel file

---
+++ lib/data/models/models.dart
@@ -0,0 +1,8 @@
+export 'chip_item.dart';
+export 'cuisine_category.dart';
+export 'dish.dart';
+export 'featured_outlet.dart';
+export 'remote/remote_models.dart';
+export 'screen_arguments.dart';
+export 'tip.dart';
+export 'wallet_option.dart';
