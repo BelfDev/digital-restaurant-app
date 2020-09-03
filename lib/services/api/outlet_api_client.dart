b8cfc472b2c8480120c563edccc27273abd1347b
Pedro Belfort <belfortdev@gmail.com>
2020-09-03 16:35:40 -0300

    Add getOutletProducts and getOutletFeaturedProducts

--- lib/services/api/outlet_api_client.dart
+++ lib/services/api/outlet_api_client.dart
@@ -1,8 +1,7 @@
 import 'dart:convert';
 
 import 'package:dr_app/configs/api_config.dart';
-import 'package:dr_app/data/models/remote/outlet.dart';
-import 'package:dr_app/data/models/remote/results.dart';
+import 'package:dr_app/data/models/models.dart';
 import 'package:flutter/foundation.dart';
 
 import 'base_api_client.dart';
@@ -10,7 +9,6 @@ import 'base_api_client.dart';
 /// Service client layer for accessing the /outlets API resource.
 /// Enables fetching all outlets, all featured outlets, or
 /// a specific outlet by id.
-// TODO: Generify GET operations
 class OutletApiClient extends BaseApiClient {
   static const _PATH = ApiConfig.OUTLETS_RESOURCE;
   static const _FEATURED_OUTLETS_PATH = '/featured';
@@ -18,20 +16,32 @@ class OutletApiClient extends BaseApiClient {
   Future<Results<Outlet>> getOutlets([String city]) async {
     Object params = _generateQueryParams(city);
     final responseBody = await getRequest(_PATH, params);
-    return compute(_parseResults, responseBody);
+    return compute(_parseOutletResults, responseBody);
   }
 
   Future<Results<Outlet>> getFeaturedOutlets([String city]) async {
     Object params = _generateQueryParams(city);
     final responseBody =
         await getRequest(_PATH + _FEATURED_OUTLETS_PATH, params);
-    return compute(_parseResults, responseBody);
+    return compute(_parseOutletResults, responseBody);
   }
 
   Future<Results<Outlet>> getOutletById(int id) async {
     assert(id >= 0, 'Outlet ID must be a positive value');
     final responseBody = await getRequest(_PATH + '/$id');
-    return compute(_parseResults, responseBody);
+    return compute(_parseOutletResults, responseBody);
+  }
+
+  Future<Results<Product>> getOutletProducts(int id) async {
+    assert(id >= 0, 'Outlet ID must be a positive value');
+    final responseBody = await getRequest(_PATH + '/$id/products');
+    return compute(_parseProductResults, responseBody);
+  }
+
+  Future<Results<Product>> getOutletFeaturedProducts(int id) async {
+    assert(id >= 0, 'Outlet ID must be a positive value');
+    final responseBody = await getRequest(_PATH + '/$id/products/featured');
+    return compute(_parseProductResults, responseBody);
   }
 
   Object _generateQueryParams([String city]) {
@@ -39,8 +49,14 @@ class OutletApiClient extends BaseApiClient {
   }
 }
 
-/// Top-level function to transform a response body into [Results].
-Results<Outlet> _parseResults(String responseBody) {
+/// Top-level function to transform a response body into [Results<Outlet>].
+Results<Outlet> _parseOutletResults(String responseBody) {
   final parsedJson = jsonDecode(responseBody);
   return Results.fromJson(parsedJson, Outlet.fromJson);
+}
+
+/// Top-level function to transform a response body into [Results<Product>].
+Results<Product> _parseProductResults(String responseBody) {
+  final parsedJson = jsonDecode(responseBody);
+  return Results.fromJson(parsedJson, Product.fromJson);
 }
