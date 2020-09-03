854df98a4d961c66b92ce7422f433b201f6e77e0
Pedro Belfort <belfortdev@gmail.com>
2020-09-03 16:33:45 -0300

    Add new models to barrel file

--- lib/data/models/remote/remote_models.dart
+++ lib/data/models/remote/remote_models.dart
@@ -1,6 +1,10 @@
+export 'category.dart';
 export 'cuisine.dart';
 export 'image.dart';
+export 'ingredient.dart';
 export 'location.dart';
 export 'outlet.dart';
+export 'product.dart';
+export 'qr_code.dart';
 export 'response.dart';
 export 'results.dart';
