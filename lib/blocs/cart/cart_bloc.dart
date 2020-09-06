45f1627d0bab360722c83a74dde869c883c3e1bf
Pedro Belfort <belfortdev@gmail.com>
2020-09-06 16:22:58 -0300

    Create cart bloc

---
+++ lib/blocs/cart/cart_bloc.dart
@@ -0,0 +1,107 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:dr_app/blocs/blocs.dart';
+import 'package:dr_app/blocs/content_state_status.dart';
+import 'package:dr_app/data/models/models.dart';
+import 'package:dr_app/data/repositories/cart_repository.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+part 'cart_event.dart';
+part 'cart_state.dart';
+
+class CartBloc extends Bloc<CartEvent, CartState> {
+  static const id = 'cart_bloc';
+
+  final HomeBloc homeBloc;
+  final CartRepository cartRepository;
+
+  CartBloc({
+    @required this.homeBloc,
+    @required this.cartRepository,
+  })  : assert(cartRepository != null),
+        super(CartState.initial()) {
+    _homeModeSubscription = homeBloc.listenHomeMode((event) {
+      if (event.mode == HomeMode.checkedIn) {
+        add(NewCartRequested(event.outletId));
+      } else if (event.mode == HomeMode.checkedOut) {
+        add(ClearCartRequested());
+      }
+    });
+  }
+
+  StreamSubscription _homeModeSubscription;
+
+  @override
+  Stream<CartState> mapEventToState(
+    CartEvent event,
+  ) async* {
+    switch (event.runtimeType) {
+      case NewCartRequested:
+        yield* _mapNewCartRequestedToState(event);
+        break;
+      case ClearCartRequested:
+        yield* _mapClearCartRequestedToState(event);
+        break;
+      case AddToCartRequested:
+        yield* _mapAddToCartRequestedToState(event);
+        break;
+    }
+  }
+
+  Stream<CartState> _mapNewCartRequestedToState(
+    NewCartRequested event,
+  ) async* {
+    final cart = await cartRepository.createCart(outletId: event.outletId);
+    yield state.copyWith(cart: cart, outletId: event.outletId);
+  }
+
+  Stream<CartState> _mapClearCartRequestedToState(
+    ClearCartRequested event,
+  ) async* {
+    await cartRepository.clearCart();
+    yield state.copyWith(outletId: null, cart: null);
+  }
+
+  Stream<CartState> _mapAddToCartRequestedToState(
+    AddToCartRequested event,
+  ) async* {
+    final initializedState = await _initCartIfNeeded(state);
+    // Loading
+    yield initializedState.copyWith(
+      status: ContentStateStatus.loadInProgress,
+    );
+    try {
+      // Success
+      final cart = await cartRepository.addCartItem(
+          productId: event.productId, quantity: event.quantity);
+      yield initializedState.copyWith(
+        status: ContentStateStatus.loadSuccess,
+        cart: cart,
+      );
+    } catch (error) {
+      // Error
+      yield initializedState.copyWith(
+        status: ContentStateStatus.loadFailure,
+      );
+    }
+  }
+
+  Future<CartState> _initCartIfNeeded(
+    CartState state,
+  ) async {
+    if (state.cart == null) {
+      final cart = await cartRepository.fetchCart();
+      return state.copyWith(cart: cart);
+    }
+    return state;
+  }
+
+  // TODO: Evaluate best moment to cancel subscriptions
+  @override
+  Future<void> close() {
+    // _homeModeSubscription?.cancel();
+    // return super.close();
+  }
+}
