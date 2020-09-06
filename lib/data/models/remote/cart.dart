6adea9461fa4dab3c4323ce117e7a36b957f5f1a
Pedro Belfort <belfortdev@gmail.com>
2020-09-06 07:12:13 -0300

    Add cart status

--- lib/data/models/remote/cart.dart
+++ lib/data/models/remote/cart.dart
@@ -3,17 +3,25 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'cart.g.dart';
 
+enum CartStatus {
+  @JsonValue('NOT_ORDERED')
+  NOT_ORDERED,
+  @JsonValue('ORDERED')
+  ORDERED
+}
+
 @JsonSerializable()
 class Cart extends Equatable {
   final int id;
   final double subtotal;
+  final CartStatus status;
 
-  const Cart(this.id, this.subtotal);
+  const Cart(this.id, this.subtotal, this.status);
 
   static Cart fromJson(Map<String, dynamic> json) => _$CartFromJson(json);
 
   Map<String, dynamic> toJson() => _$CartToJson(this);
 
   @override
-  List<Object> get props => [id, subtotal];
+  List<Object> get props => [id, subtotal, status];
 }
--- lib/data/models/remote/cart.g.dart
+++ lib/data/models/remote/cart.g.dart
@@ -10,10 +10,49 @@ Cart _$CartFromJson(Map<String, dynamic> json) {
   return Cart(
     json['id'] as int,
     (json['subtotal'] as num)?.toDouble(),
+    _$enumDecodeNullable(_$CartStatusEnumMap, json['status']),
   );
 }
 
 Map<String, dynamic> _$CartToJson(Cart instance) => <String, dynamic>{
       'id': instance.id,
       'subtotal': instance.subtotal,
+      'status': _$CartStatusEnumMap[instance.status],
     };
+
+T _$enumDecode<T>(
+  Map<T, dynamic> enumValues,
+  dynamic source, {
+  T unknownValue,
+}) {
+  if (source == null) {
+    throw ArgumentError('A value must be provided. Supported values: '
+        '${enumValues.values.join(', ')}');
+  }
+
+  final value = enumValues.entries
+      .singleWhere((e) => e.value == source, orElse: () => null)
+      ?.key;
+
+  if (value == null && unknownValue == null) {
+    throw ArgumentError('`$source` is not one of the supported values: '
+        '${enumValues.values.join(', ')}');
+  }
+  return value ?? unknownValue;
+}
+
+T _$enumDecodeNullable<T>(
+  Map<T, dynamic> enumValues,
+  dynamic source, {
+  T unknownValue,
+}) {
+  if (source == null) {
+    return null;
+  }
+  return _$enumDecode<T>(enumValues, source, unknownValue: unknownValue);
+}
+
+const _$CartStatusEnumMap = {
+  CartStatus.NOT_ORDERED: 'NOT_ORDERED',
+  CartStatus.ORDERED: 'ORDERED',
+};
